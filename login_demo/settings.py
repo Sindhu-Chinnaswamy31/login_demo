7d928a7b691fcf7026d9feb9c59182f928a03770
Your Name <sindhucswamy@gmail.com>
2020-09-24 18:39:58 +0530

    third commit

--- login_demo/settings.py
+++ login_demo/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=465
+EMAIL_HOST_USER="sindhucswamy@gmail.com"
+EMAIL_HOST_PASSWORD="7846893547"
+EMAIL_USE_SSL=True
